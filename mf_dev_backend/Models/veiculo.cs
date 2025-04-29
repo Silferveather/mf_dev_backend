cae3f854a86163e55c1c6f11073f905824d30df5
Bruno Figueiredo <brunopereirafigueiredo@gmail.com>
2025-04-29 17:02:24 -0300

    Controller Creation and changes in the model

--- mf_dev_backend/Models/veiculo.cs
+++ mf_dev_backend/Models/veiculo.cs
@@ -9,16 +9,18 @@ namespace mf_dev_backend.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Obrigatorio informar o nome.")]
+        [Required(ErrorMessage = "Obrigatorio informar o modelo.")]
         public string Nome { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar a placa.")]
         public string Placa { get; set; }
 
-        [Required(ErrorMessage = "Obrigatorio informar o ano de fabricacao.")]
+        [Required(ErrorMessage = "Obrigatorio informar o ano de fabricação.")]
+        [Display(Name = "Ano de fabricação")]
         public int AnoFabricacao { get; set; }
 
         [Required(ErrorMessage = "Obrigatorio informar o ano do modelo.")]
+        [Display(Name = "Ano do modelo")]
         public int AnoModelo { get; set; }
     }
 }
