aa58f3239a007c825722e21abf0685435d9d5907
Bruno Figueiredo <brunopereirafigueiredo@gmail.com>
2025-04-29 16:35:06 -0300

    Config database and table Veiculo

---
+++ mf_dev_backend/Models/veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar a placa.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o ano de fabricacao.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Obrigatorio informar o ano do modelo.")]
+        public int AnoModelo { get; set; }
+    }
+}
